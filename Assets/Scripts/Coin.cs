f408dcc3b23744f16de5ea7f2f132795ac98b375
David MacLeod Mayes <d.macleodmayes@gmail.com>
2025-12-07 11:58:42 -0500

    New coin UI, Main menu, level select, pause menu

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
+    private TextMeshProUGUI coinText;
+    public int coinsToGive = 1;
+
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-            player.coins += 1;
+            player.coins += coinsToGive;
             // play coin sound effect when coin is collected at 40% volume
             SoundManager.Instance.PlaySFX("COIN", 0.4f);
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
