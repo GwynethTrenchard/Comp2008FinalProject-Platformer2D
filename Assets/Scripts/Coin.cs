7f432be2b560dfc4e307b5afeb9da91b1577885b
Gwyneth Trenchard <gwyneth.trenchard@gmail.com>
2025-12-08 22:44:04 -0500

    bug fixes for check 5

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,8 +3,14 @@ using TMPro;
 
 public class Coin : MonoBehaviour
 {
-    private TextMeshProUGUI coinText;
+
+    //public AudioClip coinClip;
     public int coinsToGive = 1;
+    private TextMeshProUGUI coinText;
+
+
+    //private TextMeshProUGUI coinText;
+    //public int coinsToGive = 1;
 
     private void Start()
     {
@@ -14,9 +20,10 @@ public class Coin : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>(); // playerController?
             player.coins += coinsToGive;
             // play coin sound effect when coin is collected at 40% volume
+            //player.PlaySFX(coinClip, 0.4f);
             SoundManager.Instance.PlaySFX("COIN", 0.4f);
             coinText.text = player.coins.ToString();
             Destroy(gameObject);
